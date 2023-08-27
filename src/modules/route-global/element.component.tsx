bcabd017f6418ee6bdee9e89e08ec08fc4c62185
Nick <96056869+softwarecomplexity@users.noreply.github.com>
2023-08-27 21:54:51 +0100

    Update element.component.tsx

--- src/modules/route-global/element.component.tsx
+++ src/modules/route-global/element.component.tsx
@@ -1,11 +1,17 @@
 import { Common } from '@modules/common';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { ContactPage, CookiesPage, PrivacyPage } from './pages';
 
 export const Element: React.FC = () => {
+    const navigate = useNavigate();
+    
+    const redirectToDiscord = () => {
+        window.location.href = 'https://discord.com/invite/diablo4'; // Redirect to Discord link
+    };
+    
     return (
         <Routes>
-            <Route path='contact' element={<ContactPage />} />
+            <Route path='contact' element={<ContactPage />} onClick={redirectToDiscord} /> {/* Add onClick handler */}
             <Route path='cookies' element={<CookiesPage />} />
             <Route path='privacy' element={<PrivacyPage />} />
             <Route path='*' index element={<Common.NotFoundPage />} />
