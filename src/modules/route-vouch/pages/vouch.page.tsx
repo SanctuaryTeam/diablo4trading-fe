cab9daad041e9a70a968b1df7eaeb6ee092658a9
Dennis Long <dennis@digitalflavors.net>
2023-09-02 16:45:13 -0700

    Forgot to refactor user-rating on vouch.page.

--- src/modules/route-vouch/pages/vouch.page.tsx
+++ src/modules/route-vouch/pages/vouch.page.tsx
@@ -61,40 +61,8 @@ export const VouchPage: React.FC = () => {
                                             xs={12}
                                             sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
                                         >
-                                            <Box
-                                                sx={{
-                                                    display: 'flex',
-                                                    mt: 2,
-                                                    justifyContent: 'space-between',
-                                                    alignItems: 'flex-end',
-                                                }}
-                                            >
-                                                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                                                    <Box>
-                                                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                                            <Grid container spacing={1}>
-                                                                <Grid item xs={12} md={6}>
-                                                                    <Avatar
-                                                                        src='https://placekitten.com/40/40'
-                                                                        sx={{ mr: 1 }}
-                                                                    />
-                                                                </Grid>
-                                                                <Grid item xs={12} md={6}>
-                                                                    <Box>
-                                                                        <Typography
-                                                                            variant='subtitle1'
-                                                                            fontWeight='bold'
-                                                                        >
-                                                                            {'SparkyOnyx#1187'}
-                                                                        </Typography>
-                                                                        <Common.UserRating user={'SparkyOnyx#1187'} rating={6} score={456} />
-                                                                    </Box>
-                                                                </Grid>
-                                                            </Grid>
-                                                        </Box>
-                                                    </Box>
-                                                </Box>
-                                            </Box>
+                                            
+                                            <Common.UserRating user={'SparkyOnyx#1187'} rating={6} score={456} />
                                         </Grid>
                                     </Grid>
                                 </Card>
