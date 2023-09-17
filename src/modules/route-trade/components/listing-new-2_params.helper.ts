c9994e2e455a4d351b71cb3a4e582b492387b2fd
Devoder <devoder@d4search.com>
2023-09-16 22:39:40 -0400

    Finish strict checks before review

--- src/modules/route-trade/components/listing-new-2_params.helper.ts
+++ src/modules/route-trade/components/listing-new-2_params.helper.ts
@@ -3,6 +3,6 @@ import { ListingNewParamsFormValue } from './listing-new-2_params.types';
 
 export const isListingNewParamsFormValid = (
     form: ListingNewParamsFormValue,
-): boolean => form.language !== undefined && form.serverType !== undefined && Object.values(Game.Language).includes(form.language)
-        && Object.values(Game.ServerType).includes(form.serverType);
-
+): boolean =>
+    form.language !== undefined && form.serverType !== undefined && Object.values(Game.Language).includes(form.language)
+    && Object.values(Game.ServerType).includes(form.serverType);
